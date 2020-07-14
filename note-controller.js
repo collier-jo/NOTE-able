93570f38359dfdca4c99496f336e0c5a792a86e7
joannacollier <collj035@gmail.com>
2020-07-14 12:48:09 +0100

    spiked controller

--- note-controller.js
+++ note-controller.js
@@ -1,7 +1,42 @@
+"use strict";
+
+(function(exports) {
 
 
-(function(){
-  var element = document.getElementById("app");
-  element.innerHTML = "Howdy"
-})();
+  function NoteController(list = new NoteList){
+    this.list = list
+  };
+  
+  NoteController.prototype.add = function(text){
+    this.list.create(text)
+  };
 
+  NoteController.prototype.view = function () {
+    var testing = this.list.convertAllToHTML()
+    console.log(testing)
+    return testing
+  }
+
+  NoteController.prototype.addToPage = function() {
+    var test2 = document.getElementById("app")
+    test2.innerHTML = this.view()
+  }
+ 
+  exports.NoteController = NoteController;
+
+})(this);
+
+var test = new NoteController
+test.add("Favourite drink: seltzer.")
+test.add("Testing ")
+test.add("Testing 2")
+test.view()
+test.addToPage()
+
+
+
+// Takes a note list model as a parameter. √
+// Adds a note that says Favourite drink: seltzer. √
+// Creates a note list view, passing in the note list model.
+// The note controller has a method that gets HTML from the 
+// note list view and inserts it into the app element.
