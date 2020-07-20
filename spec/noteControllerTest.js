10e1e0b2e9790a7ef0613bfef711a12b1b69407b
joannacollier <collj035@gmail.com>
2020-07-20 16:54:03 +0100

    Has been called matcher created

--- spec/noteControllerTest.js
+++ spec/noteControllerTest.js
@@ -1,29 +1,33 @@
-// A note controller can be instantiated.
-// The innerHTML property of the app element 
-// contains HTML somewhat similar to: 
-// <ul><li><div>Favourite food: pesto</div></li></ul>. 
-// (You'll need to mock some stuff to achieve this.)
-
-// const { NoteController } = require("../note-controller")
 
 console.log("Controller Spec")
 
-var noteListdouble = {
-  create: function(text){
-    return text
-  },
-  convertAllToHTML: function(){
-   return "<ul><li><div>Hello</div></li></ul>"
-  }
-};
 
-var controllerTest = new NoteController(noteListdouble)
+
+
 
 it("Should add to the list array", function(){
-  expect(controllerTest.add("Hello")).toEq("Hello");
+  var noteListdouble = {
+    create: function(text){
+      hasBeenCalled[create] = 1
+    },
+  };
+
+  var controllerTest = new NoteController(noteListdouble)
+  controllerTest.add("Hello")
+  expect(noteListdouble.create).toHaveBeenCalledWith("Hello");
 }); 
 
 
-it("Should fill the innerHTML", function(){
-  expect(controllerTest.addToPage()).toEq("<ul><li><div>Hello</div></li></ul>")
+it("CovertAllToHTML is called", function(){
+  var noteListdouble = {
+    convertAllToHTML: function(){
+      hasBeenCalled["convertAllToHTML"] = 1
+    }
+  };
+
+  // spyOn(noteListdouble, "convertAllToHTML")
+
+  var controllerTest = new NoteController(noteListdouble)
+  controllerTest.addToPage()
+  expect(noteListdouble.convertAllToHTML).toHaveBeenCalled()
 })
--- spec/test-framework.js
+++ spec/test-framework.js
@@ -1,15 +1,28 @@
+var hasBeenCalled = {}
+
 function it(label, callback) {
     console.log("Test: " + label)
     callback()
   }
+
+  // spy on function 
+  // function spyOn(obj, myFunction){
+
+  //   var unicorn = function() {
+  //     hasBeenCalled[myFunction] = 1
+  //   }
+
+  //   obj[myFunction] = unicorn
+
+  // }
   
   function expect(a){
     return {
       toEq: function(b){
         if(a === b){
-            console.log("Pass")
+          console.log("Pass")
         }else{
-            console.log("Fail")
+          console.log("Fail")
         }
       },
   
@@ -17,12 +30,18 @@ function it(label, callback) {
         if(a.includes(b)){
           console.log("Pass")
         }else{
-            console.log("Fail")
+          console.log("Fail")
         }
       },
 
-    //   isNode: 
-
-    //   isHTML 
+      toHaveBeenCalled: function(){
+        console.log(hasBeenCalled)
+        console.log(a.name)
+        if(hasBeenCalled[a.name] === 1){
+          console.log("Pass")
+        } else {
+          console.log("Fail")
+        }
+      }
     }
   }
