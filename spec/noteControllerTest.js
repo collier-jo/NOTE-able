b185a4b60a0162973a6c4fd2c61b34db28802138
joannacollier <collj035@gmail.com>
2020-07-21 19:51:36 +0100

    double for document added

--- spec/noteControllerTest.js
+++ spec/noteControllerTest.js
@@ -15,6 +15,12 @@ it("Create is called", function(){
 
 
 it("CovertAllToHTML is called", function(){
+  const saveGetElementById = document.getElementById
+  let doubleDiv = {innerHTML: undefined}
+  document.getElementById = () => doubleDiv
+
+
+
   var noteListdouble = {
     convertAllToHTML: function(){
       hasBeenCalled["convertAllToHTML"] = 1
@@ -24,4 +30,27 @@ it("CovertAllToHTML is called", function(){
   var controllerTest = new NoteController(noteListdouble)
   controllerTest.addToPage()
   expect(noteListdouble.convertAllToHTML).toHaveBeenCalled()
+
+  document.getElementById = saveGetElementById
+
+
 })
+
+// it("Adds text to innerHTML", function(){
+//   const saveGetElementById = document.getElementById("app")
+//   let doubleDiv = {innerHTML: undefined}
+//   document.getElementById = () => doubleDiv
+
+//   noteListdouble = {
+//     convertAllToHTML: function(){
+//       return "<ul><li><div>Hello</div></li></ul>"
+//     }
+//   }
+
+//   var controllerTest = new NoteController(noteListdouble)
+
+//   // expect(controllerTest.addToPage()).toEq("Hello")
+//   console.log(saveGetElementById)
+   
+//   document.getElementById = saveGetElementById
+// })
